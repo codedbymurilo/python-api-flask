63599d14b2414482fcfd31715ec990b9f770578f
devMuriloNeto <murilonunesneto@email.com>
2026-05-05 10:44:41 -0300

    primeiro comitt/ rota get livros

---
+++ app.py
@@ -0,0 +1,45 @@
+# API - É um lugar para disponibilizar recursos e/ou funcionalidades
+# 1. Objetivo - Criar um api que disponibiliza a consulta, criação, edição e exclusão de livros.
+# 2. URL base - localhost
+# 3. Endpoints - 
+    # - localhost/livros(GET)
+    # - localhost/livros/id(POST)
+    # - localhost/livros/id(GET)
+    # - localhost/livros/id(PUT)
+    # - localhost/livros/id(DELETE)
+# 4. Quais recursos - Livros
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'titulo': 'o Senhor dos Anéis - A Sociedade do Anel',
+        'autor': 'J.R.R Tolkien'
+    },
+    {
+        'id': 2,
+        'titulo': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K Howling'
+    },
+    {
+        'id': 3,
+        'titulo': 'A Guerra dos Tronos',
+        'autor': 'George R.R. Martin'
+    }
+]
+
+# Consultar(todo)
+@app.route('/livros')
+def obter_livros():
+    return jsonify(livros)
+
+# Consultar(id)
+
+# Editar
+
+# Excluir
+
+app.run(port=5000,host='localhost',debug=True)
