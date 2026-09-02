5a673c50b59475d197915a175fa1b606232db3c6
Murilo Neto <124456441+codedbymurilo@users.noreply.github.com>
2026-09-02 09:19:13 -0300

    Fix route parameter name for deleting books

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def incluir_novo_livro():
     return jsonify(livros) # para retornar todos os livros e verificar que foi criado
 
 # Excluir
-@app.route('/livros/<int:od>',methods=['DELETE'])
+@app.route('/livros/<int:id>',methods=['DELETE'])
 def excluir_livros(id):
     for indice,livro in enumerate(livros):
         if livro.get('id') == id:
